a2f74f888a6bb22ff13314173830706e1399faf7
Andrew Gallant <Andrew.Gallant@tufts.edu>
2013-11-14 14:04:24 -0500

    reorg

--- tests/A.hs
+++ tests/A.hs
@@ -8,6 +8,24 @@ import qualified System.IO as I
 import Development.Fex.Experiment
 import Development.Fex.IO
 
+main = do
+  deps <- dependsExper (dummy >> dummy)
+  print deps
+
+  -- d <- evalExper dummy 
+  -- print d 
+
+  -- deps <- dependsExper test1 
+  -- print deps 
+  -- effs <- effectsExper test1 
+  -- print effs 
+--  
+  -- hmm <- evalExper test1 
+  -- print hmm 
+  -- handle <- evalExper test1 
+  -- I.hPutStrLn handle "test test" 
+  -- I.hClose handle 
+
 test1 :: Experiment ()
 test1 = do
   depend (DExec "ls", "list files")
@@ -18,22 +36,4 @@ test1 = do
   -- s <- experIO $ I.hGetContents handle 
   -- depend (DExec s, "tricky") 
   -- return s 
-
-main = do
-  -- deps <- dependsExper (dummy >> dummy) 
-  -- print deps 
---  
-  -- d <- evalExper dummy 
-  -- print d 
-
-  deps <- dependsExper test1
-  print deps
-  effs <- effectsExper test1
-  print effs
-
-  hmm <- evalExper test1
-  print hmm
-  -- handle <- evalExper test1 
-  -- I.hPutStrLn handle "test test" 
-  -- I.hClose handle 
 
